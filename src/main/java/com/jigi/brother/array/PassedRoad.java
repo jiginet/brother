f9399a06157015853b0b11d75532f92f726d2575
jigi <jigi@jigi.net>
2020-08-16 20:53:11 +0900

    ㅇ "방문길이" 클래스를 이용한 리팩토링

--- src/main/java/com/jigi/brother/array/PassedRoad.java
+++ src/main/java/com/jigi/brother/array/PassedRoad.java
@@ -5,73 +5,91 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class PassedRoad {
-    private final Set<String> passedRoad = new HashSet<>();
-    private final Point currentLocation = new Point(5, 5);
 
     public int solution(String dirs) {
 
+        Point currentPoint = new Point(5, 5);
+        Set<Path> visitedPath = new HashSet<>();
         for (char direction : dirs.toCharArray()) {
-            move(currentLocation, direction);
+            Path path = movePoint(currentPoint, direction);
+            if (path.isMoving()) visitedPath.add(path);
         }
 
-        return passedRoad.size();
+        return visitedPath.size();
     }
 
-    private void move(Point location, char direction) {
+    private Path movePoint(Point currentPoint, char direction) {
+        Point sourcePoint = new Point(currentPoint.x, currentPoint.y);
+        int x = 0, y = 0;
         switch (direction) {
             case 'U':
-                goingUp(location);
+                x = -1;
                 break;
             case 'D':
-                goingDown(location);
+                x = 1;
                 break;
             case 'L':
-                goingLeft(location);
+                y = -1;
                 break;
             case 'R':
-                goingRight(location);
+                y = 1;
                 break;
         }
-    }
 
-    private void goingUp(Point destination) {
-        Point source = new Point(destination.x, destination.y);
-        if (source.x > 0) {
-            destination.x--;
+        if (currentPoint.x + x >= 0 && currentPoint.x + x <= 10) {
+            currentPoint.x += x;
         }
-        addPath(source, destination);
-    }
 
-    private void goingDown(Point destination) {
-        Point source = new Point(destination.x, destination.y);
-        if (source.x < 10) {
-            destination.x++;
+        if (currentPoint.y + y >= 0 && currentPoint.y + y <= 10) {
+            currentPoint.y += y;
         }
-        addPath(source, destination);
+
+        return new Path(sourcePoint, new Point(currentPoint.x, currentPoint.y));
     }
 
-    private void goingLeft(Point destination) {
-        Point source = new Point(destination.x, destination.y);
-        if (source.y > 0) {
-            destination.y--;
+    private class Path {
+        private Point source;
+        private Point destination;
+
+        public Path(Point source, Point destination) {
+            this.source = source;
+            this.destination = destination;
         }
-        addPath(source, destination);
-    }
 
-    private void goingRight(Point destination) {
-        Point source = new Point(destination.x, destination.y);
-        if (source.y < 10) {
-            destination.y++;
+        public boolean isMoving() {
+            if (source.x != destination.x || source.y != destination.y) return true;
+            return false;
         }
-        addPath(source, destination);
-    }
 
-    private void addPath(Point source, Point destination) {
-        if (source.x != destination.x || source.y != destination.y) {
-            String sourcePath = String.format("%02d", source.x) +  String.format("%02d", source.y);
-            String destinationPath = String.format("%02d", destination.x) +  String.format("%02d", destination.y);
-            String newPath = sourcePath.compareTo(destinationPath) == -1 ? sourcePath + destinationPath : destinationPath + sourcePath;
-            passedRoad.add(newPath);
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) return false;
+            if (obj == this) return true;
+            if (obj.getClass() != this.getClass()) return false;
+
+            Path that = (Path) obj;
+
+            if (this.source != null && that.source == null || this.source == null && that.source != null) return false;
+            if (this.destination != null && that.destination == null || this.destination == null && that.destination != null)
+                return false;
+
+            if (this.hashCode() == that.hashCode()) {
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+
+            final int prime = 31;
+            int hashCode = 1;
+
+            hashCode = prime * hashCode + (source.x + destination.x);
+            hashCode = prime * hashCode + (source.y + destination.y);
+
+            return hashCode;
         }
     }
 }
