0c0df99119efd98c95d0216fce8b1f4a2bc667e3
jigi <jigi@jigi.net>
2020-08-18 23:39:01 +0900

    ㅇ "방문길이" 리팩토링

--- src/main/java/com/jigi/brother/array/PassedRoad.java
+++ src/main/java/com/jigi/brother/array/PassedRoad.java
@@ -1,95 +1,97 @@
 package com.jigi.brother.array;
 
-import java.awt.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class PassedRoad {
 
-    public int solution(String dirs) {
+    private class Point {
+        private final int x;
+        private final int y;
 
-        Point currentPoint = new Point(5, 5);
-        Set<Path> visitedPath = new HashSet<>();
-        for (char direction : dirs.toCharArray()) {
-            Path path = movePoint(currentPoint, direction);
-            if (path.isMoving()) visitedPath.add(path);
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
 
-        return visitedPath.size();
-    }
+        public Point movePoint(char direction) {
+            int x = 0, y = 0;
+            switch (direction) {
+                case 'U':
+                    x = -1;
+                    break;
+                case 'D':
+                    x = 1;
+                    break;
+                case 'L':
+                    y = -1;
+                    break;
+                case 'R':
+                    y = 1;
+                    break;
+            }
 
-    private Path movePoint(Point currentPoint, char direction) {
-        Point sourcePoint = new Point(currentPoint.x, currentPoint.y);
-        int x = 0, y = 0;
-        switch (direction) {
-            case 'U':
-                x = -1;
-                break;
-            case 'D':
-                x = 1;
-                break;
-            case 'L':
-                y = -1;
-                break;
-            case 'R':
-                y = 1;
-                break;
+            int nextX = this.x;
+            if (this.x + x >= 0 && this.x + x <= 10) {
+                nextX += x;
+            }
+
+            int nextY = this.y;
+            if (this.y + y >= 0 && this.y + y <= 10) {
+                nextY += y;
+            }
+
+            return new Point(nextX, nextY);
         }
 
-        if (currentPoint.x + x >= 0 && currentPoint.x + x <= 10) {
-            currentPoint.x += x;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x && y == point.y;
         }
 
-        if (currentPoint.y + y >= 0 && currentPoint.y + y <= 10) {
-            currentPoint.y += y;
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
-
-        return new Path(sourcePoint, new Point(currentPoint.x, currentPoint.y));
     }
 
     private class Path {
-        private Point source;
-        private Point destination;
+        private final Point source;
+        private final Point destination;
 
         public Path(Point source, Point destination) {
             this.source = source;
             this.destination = destination;
         }
 
-        public boolean isMoving() {
-            if (source.x != destination.x || source.y != destination.y) return true;
-            return false;
-        }
-
         @Override
-        public boolean equals(Object obj) {
-            if (obj == null) return false;
-            if (obj == this) return true;
-            if (obj.getClass() != this.getClass()) return false;
-
-            Path that = (Path) obj;
-
-            if (this.source != null && that.source == null || this.source == null && that.source != null) return false;
-            if (this.destination != null && that.destination == null || this.destination == null && that.destination != null)
-                return false;
-
-            if (this.hashCode() == that.hashCode()) {
-                return true;
-            }
-
-            return false;
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Path path = (Path) o;
+            return this.hashCode() == path.hashCode();
         }
 
         @Override
         public int hashCode() {
-
-            final int prime = 31;
-            int hashCode = 1;
-
-            hashCode = prime * hashCode + (source.x + destination.x);
-            hashCode = prime * hashCode + (source.y + destination.y);
-
-            return hashCode;
+            return source.hashCode() + destination.hashCode();
         }
+    }
+
+    public int solution(String dirs) {
+
+        Point currentPoint = new Point(5, 5);
+        Set<Path> visitedPath = new HashSet<>();
+        for (char direction : dirs.toCharArray()) {
+            Point nextPoint = currentPoint.movePoint(direction);
+            if (!currentPoint.equals(nextPoint)) visitedPath.add(new Path(currentPoint, nextPoint));
+            currentPoint = nextPoint;
+        }
+
+        return visitedPath.size();
     }
 }
