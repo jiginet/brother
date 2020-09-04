99636b0144802983a178ac7019c14962c0613ae4
jigi <jigi@jigi.net>
2020-09-04 21:54:53 +0900

    ㅇ 타겟넘버 문제풀이

--- src/main/java/com/jigi/brother/array/TargetNumber.java
+++ src/main/java/com/jigi/brother/array/TargetNumber.java
@@ -1,9 +1,27 @@
 package com.jigi.brother.array;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TargetNumber {
-
-
     public int solution(int[] numbers, int target) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(numbers[0]);
+        queue.offer(-numbers[0]);
+        for (int i = 1; i < numbers.length; i++) {
+            int queueSize = queue.size();
+            for (int j = 0; j < queueSize; j++) {
+                int num = queue.poll();
+                queue.offer(num + numbers[i]);
+                queue.offer(num - numbers[i]);
+            }
+        }
+
+        return (int) queue.stream().filter(i -> i.equals(target)).count();
+    }
+
+    public int solution2(int[] numbers, int target) {
 
         int answer = 0;
         int number = numbers[0];
